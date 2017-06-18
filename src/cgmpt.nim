4d80eecc92864829c489a5eb7ffe79372b48b2e1
copygirl <halfnerd.copygirl@gmail.com>
2017-06-18 18:14:17 +0200

    Formatting

--- src/cgmpt.nim
+++ src/cgmpt.nim
@@ -1,32 +1,31 @@
 import sdl2
 
 try:
-    sdl2.init(sdl2.INIT_VIDEO + sdl2.INIT_AUDIO + sdl2.INIT_EVENTS)
+  sdl2.init(sdl2.INIT_VIDEO + sdl2.INIT_AUDIO + sdl2.INIT_EVENTS)
 except Exception:
-    echo getCurrentExceptionMsg()
-
+  echo getCurrentExceptionMsg()
 
 let window = sdl2.createWindow(
-    title = "CGMPT", 
-    x = SDL_WINDOWPOS_CENTERED, 
-    y = SDL_WINDOWPOS_CENTERED, 
-    w = 800, 
-    h = 600, 
-    flags = SDL_WINDOW_SHOWN + SDL_WINDOW_OPENGL
+  title = "CGMPT",
+  x = SDL_WINDOWPOS_CENTERED,
+  y = SDL_WINDOWPOS_CENTERED,
+  w = 800,
+  h = 600,
+  flags = SDL_WINDOW_SHOWN + SDL_WINDOW_OPENGL
 )
 
 var running = true
 
 proc pollEvent =
-    var event: sdl2.Event
-    while sdl2.pollEvent(event):
-        case event.kind
-        of sdl2.QuitEvent:
-            running = false
-        else: discard
+  var event: sdl2.Event
+  while sdl2.pollEvent(event):
+    case event.kind
+    of sdl2.QuitEvent:
+      running = false
+    else: discard
 
 while running:
-    pollEvent()
+  pollEvent()
 
 echo "Terminating!"
 
